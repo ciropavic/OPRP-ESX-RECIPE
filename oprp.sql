8a1b7908e39aaa0c03389c4df34c895ed614addc
Jacob Paulin <paul0319@algonquinlive.com>
2021-12-13 16:03:05 -0500

    Fixes some errors in sql

--- oprp.sql
+++ oprp.sql
@@ -207,15 +207,7 @@ SET time_zone = "+00:00";
 
     -- DDL
 
-    CREATE TABLE IF NOT EXISTS `owned_vehicles` (
-        `owner` varchar(40) NOT NULL,
-        `plate` varchar(12) NOT NULL,
-        `vehicle` longtext,
-        `type` varchar(20) NOT NULL DEFAULT 'car',
-        `job` varchar(20) NOT NULL DEFAULT 'civ',
-        `stored` tinyint(1) NOT NULL DEFAULT '0',
-        PRIMARY KEY (`plate`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+    -- N/A
 
 
     -- DML
