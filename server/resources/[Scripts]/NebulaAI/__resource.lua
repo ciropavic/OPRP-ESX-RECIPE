0d0e80a7f29e002cfd3e054cc96f8473cae15fb0
Jacob Paulin <jacobwpaulin@gmail.com>
2022-01-05 17:59:46 -0500

    Changes

--- server/resources/[Scripts]/NebulaAI/__resource.lua
+++ server/resources/[Scripts]/NebulaAI/__resource.lua
@@ -1,7 +1,7 @@
-author "Jay (Error#3569)"
-
-client_script "traffic.lua"
-client_script "client.lua"
+client_scripts {
+    "traffic.lua",
+    "client.lua"
+}
 
 data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'FIVEM_LOVES_YOU_4B38E96CC036038F' 'events.meta'
